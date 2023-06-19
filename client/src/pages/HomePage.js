54a6e50a8fa3b2b60be1a710bf56fcb1c77867d2
maurice <maurice.oluoch@naiparq.co.ke>
2023-06-19 16:59:34 +0300

    first commit

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,23 @@
+import JobList from "../components/JobList";
+import { getJobs } from "../lib/graphql/queries";
+import { useState, useEffect } from "react";
+
+// getJobs().then((jobs) => console.log("Right data =>", jobs));
+
+function HomePage() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    getJobs().then((jobs) => setJobs(jobs));
+    // getJobs().then(setJobs);
+  }, []);
+
+  return (
+    <div>
+      <h1 className="title">Job Board</h1>
+      <JobList jobs={jobs} />
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ server/resolvers.js
@@ -0,0 +1,16 @@
+import { getJobs } from "./db/jobs.js";
+import { getCompany } from "./db/companies.js";
+
+export const resolvers = {
+  Query: {
+    jobs: () => getJobs(),
+  },
+  Job: {
+    company: (job) => getCompany(job.companyId),
+    date: (job) => toIsoDate(job.createdAt),
+  },
+};
+
+const toIsoDate = (value) => {
+  return value.slice(0, "yyyy-mm-dd".length);
+};
